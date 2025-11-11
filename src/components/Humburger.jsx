6d2938f10337125a2b97ff25efc689ccdbcc951e
TomserAliKhan <tomserali6500@gmail.com>
2025-11-11 21:49:51 +0530

    a

--- src/components/Humburger.jsx
+++ src/components/Humburger.jsx
@@ -2,10 +2,26 @@ import React, { useEffect, useRef } from "react";
 import { FaXmark } from "react-icons/fa6";
 import { Link, NavLink } from "react-router-dom";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export const Humburger = ({links,setHum}) => {
 
+useGSAP(()=>{
+  let tl=gsap.timeline()
+  tl.from('#box',{
+    y:-300,
+    duration:0.5,
+    opacity:0
+  })
+  .from('.hi,#x',{
+    y:-20,
+    delay:0.1,
+    opacity:0,
+    duration:0.5,
+    stagger:0.1
+  })
 
+})
 
 
 
@@ -15,8 +31,8 @@ export const Humburger = ({links,setHum}) => {
 
    let clas='hover:text-orange-600 hi'
   return (
-    <div className=" absolute w-[100%] h-[300px] left-0 top-[-540px]  bg-[#323331]   "  >
-    <div className="flex flex-col items-center  justify-center gap-4 text-2xl">
+    <div id="box" className=" absolute w-[102%]  h-[300px] left-[-8px] top-[-10px]  bg-[#323331]   "  >
+    <div  className=" flex flex-col items-center  justify-center gap-4 text-2xl">
       <Link onClick={()=>setHum(true) }>
         <FaXmark size='30px'  className=" absolute right-[28px] top-[13px]" />
         </Link>
@@ -35,7 +51,7 @@ export const Humburger = ({links,setHum}) => {
       })
       }
     </div>
-    <div className="h-60  " onClick={()=>setHum(true)}>
+    <div className="h-60  " id="x" onClick={()=>setHum(true)}>
 
     </div>
     </div>
--- src/components/routes/About.jsx
+++ src/components/routes/About.jsx
@@ -8,9 +8,10 @@ const About = () => {
 
   
   return (
-    <div className=' flex flex-col items-center justify-center p-5'>
+    <div className=' flex flex-col items-center justify-center p-5 overflow-x-hidden'>
 
-       <h1 style={{lineHeight:"47px"}} className='text-5xl leading-8  font-extrabold text-[#2DE5E8]  '>Frontend Devloper</h1>
+       <h1 style={{lineHeight:"43px"}} className='text-[45px] translate-x-[-25px] leading-8  font-extrabold text-[#2DE5E8] self-start '>&lt;Frontend&gt;<br />
+       <span className='ms-[2rem]'>&lt;&#47;Devloper&gt;</span> </h1>
         <p className='my-6 text-[19px] text-yellow-50 '>
            I build fast, accessible, and polished web interfaces using React, Tailwind CSS and modern JavaScript. I focus on UX-led designs and clean, maintainable code.
         </p>
