d0ec3e9f9778a3c99fa365837558a1ed59a32fed
TAK <tomserali6500@gmail.com>
2026-06-20 07:49:35 +0530

    Change header text color to gray

--- src/components/routes/Class12.jsx
+++ src/components/routes/Class12.jsx
@@ -16,7 +16,7 @@ const subjects = [
 function Class12() {
   return (
     <div className="max-w-6xl mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">
+      <h1 className="text-3xl font-bold mb-6 text-gray-800 ">
         Class 12 Arts Question Papers
       </h1>
 
